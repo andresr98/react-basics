34a76a98171e0246e0c1f312ee2a3bacac6b7249
Andres Ruiz Graciano <aandres.r98@gmail.com>
2020-07-10 13:19:06 -0500

    Using React as a Single Page Application Part 2 and to send path params to a component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if (dish != null) {
             return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
             );
         } else {
             return (
@@ -22,25 +25,22 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
     function RenderComments({comments}) {
 
         if (comments != null) {
-            const commetsHtml = comments.map((comment) => {
-                return (
-                    <div key={comment.id}>
-                        <li>{comment.comment}</li>
-                        <br/>
-                        <li> -- {comment.author} , {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                        <br/>
-                    </div>
-                );
-            });
-
-            return (
-                <div>
+            return(
+                <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {commetsHtml}
+                        {/** To return other JSX Element inside a return */}
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
-            );
+            );  
         } else {
             return (
                 <div></div>
@@ -52,12 +52,18 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
         return(
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish}/>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish ? props.dish.comments : null} />
-                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish}/>
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
